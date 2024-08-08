e09820445b09479fe85f041a2ace49cc33673f04
Jreff6 <enzo.solaar@hotmail.fr>
2024-08-08 17:41:16 +0200

    progress on the auth api + book api

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+
+const app = express();
+
+const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
+
+
+
+app.use(express.json());  
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connecté à MongoDB'))
+.catch(err => console.error('Erreur de connexion à MongoDB', err));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.use('/api/stuff', bookRoutes);
+  app.use('/api/auth', userRoutes);
+
+  module.exports= app;
